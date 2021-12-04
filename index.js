5c183f4e2d225fe20cd49e9b57cc05bac1f2ad62
dionysus0214 <hiba0214@naver.com>
2021-12-04 21:46:46 +0900

    typescript 기초 문법

---
+++ index.js
@@ -0,0 +1,16 @@
+var 이름 = 'kim';
+var 이름2 = 123;
+var 이름들 = ['kim', 'park'];
+var 오브젝트 = { name: 'kim' };
+function 함수(x) {
+    return x * 2;
+}
+var john = [123, true];
+var johes = { name: 'kim', age: '12' };
+// class
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    return Person;
+}());
---
+++ index.ts
@@ -0,0 +1,26 @@
+type Mytype = string | number;
+let 이름: string = 'kim';
+let 이름2: Mytype = 123;
+let 이름들: string[] = ['kim', 'park'];
+let 오브젝트: { name: string } = { name: 'kim' };
+
+function 함수(x: number): number {
+  return x * 2;
+}
+
+// tuple
+type Member = [number, boolean];
+let john: Member = [123, true];
+
+type Members = {
+  [key: string] :string, // [key: string]는 모든 오브젝트 속성, 글자로 된 모든 object 속성의 타입은 string
+}
+let johes: Members = { name: 'kim', age: '12' };
+
+// class
+class Person {
+  name;
+  constructor(name :string){
+    this.name = name;
+  }
+}
